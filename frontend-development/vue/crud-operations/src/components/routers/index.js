703f27031056ea3f2234caa64a24f90ebbee520d
dina <dinaelhadidy966@gmail.com>
2025-02-23 23:51:25 +0200

    Add a component to catch all invalid routes

--- frontend-development/vue/crud-operations/src/components/routers/index.js
+++ frontend-development/vue/crud-operations/src/components/routers/index.js
@@ -4,13 +4,15 @@ import About from "../views/about.vue";
 import Contact from "../views/contact.vue";
 import Dynamic from "../views/dynamic.vue";
 import studentDetails from "../views/studentDetails.vue";
+import UnderConstuction from "../views/underConstuction.vue";
 
 const routes = [
     {path: '/', component: MyApp},
     {path: '/about', component: About},
     {path: '/contact', component: Contact},
-    {path: '/dynamic', component: studentDetails},
-    {path: '/student/:id', component: Dynamic},
+    {path: '/dynamic', component: Dynamic},
+    {path: '/student/:id', component: studentDetails},
+    {path: '/:catchAll(.*)', component: UnderConstuction},
 ]
 
 export const router = createRouter({
