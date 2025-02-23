dd5db63b39ac3d03c76f5c567cde8fe0e32a186c
dina <dinaelhadidy966@gmail.com>
2025-02-23 23:50:56 +0200

    Create landing page and configure routes

--- frontend-development/vue/crud-operations/src/components/landing.vue
+++ frontend-development/vue/crud-operations/src/components/landing.vue
@@ -1,16 +1,16 @@
 <template>
-    <div>
-        <h4 class="p-5">landing page</h4>
+    <div class="container my-2">
+        <h4 class="text-center p-5">landing page</h4>
         <div class="row border border-primary g-0">
             <ul class="d-flex list-unstyled justify-content-evenly align-items-center">
-                <li><a href="#" @click.prevent="targetCompontent = 'home'">Home</a></li>
-                <li><a href="#" @click.prevent="targetCompontent = 'contact'">Contact</a></li>
-                <li><a href="#" @click.prevent="targetCompontent = 'about'">About</a></li>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/contact">Contact</router-link></li>
+                <li><router-link to="/about">About</router-link></li>
+                <li><router-link to="/dynamic">Dynamic</router-link></li>
             </ul>
         </div>
         <div class="row border border-warning g-0 py-5">
-            <h4 class="text-center">dynamic</h4>
-
+            <router-view></router-view>
         </div>
     </div>
 </template>
--- frontend-development/vue/crud-operations/src/main.js
+++ frontend-development/vue/crud-operations/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import myApp from "@/myApp";
 import dynamic from "./components/views/dynamic.vue";
+import landing from "./components/landing.vue";
+import { router } from "./components/routers";
 
 // createApp(myApp).mount("#app");
-createApp(dynamic).mount("#app");
+// createApp(dynamic).mount("#app");
+createApp(landing).use(router).mount("#app");
+
