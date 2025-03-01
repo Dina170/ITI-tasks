9d224fcfb761d5a6792a59cd5bb0a18b957dd6e5
dina <dinaelhadidy966@gmail.com>
2025-03-01 15:36:33 +0200

    Create products list and product components

---
+++ frontend-development/vue/cart/src/components/product.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card">
+        <div>
+            <img :src="product.image" class="card-img-top">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p>{{ product.description }}</p>
+        </div>
+        <div class="card-footer d-flex justify-content-between align-items-baseline">
+            <p class="pt-3"><span
+                    :class="[product.instock >= 5 ? 'more' : '', product.instock < 5 ? 'less' : '', product.instock == 0 ? 'none' : '']">instock:
+                    {{ product.instock }}</span></p>
+            <button class="btn btn-primary" @click="cartStore.addToCart(product)" :disabled="product.instock == 0">Add
+                to
+                Cart</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import { useCartStore } from '@/stores/cartStore';
+
+import { defineProps } from 'vue';
+
+const cartStore = useCartStore();
+
+const props = defineProps(['product']);
+
+
+</script>
+
+<style></style>
---
+++ frontend-development/vue/cart/src/components/productsList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row justify-content-between align-items-center my-5 mx-1">
+        <div class="col-4 mb-2" v-for="product in productStore.products" :key="product.id">
+            <product :product="product" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useProductStore } from '@/stores/productStore';
+
+import product from './product.vue';
+
+const productStore = useProductStore();
+
+onMounted(() => {
+    productStore.loadProducts();
+});
+
+</script>
+
+<style></style>
