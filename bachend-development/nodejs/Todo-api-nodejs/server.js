c4e9c55de9da7b559ada1e708bc0bd3dccc3381e
dina <dinaelhadidy966@gmail.com>
2025-04-10 23:30:18 +0200

    Refactor Todo API to use asynchronous file operations for reading and writing data

--- bachend-development/nodejs/Todo-api-nodejs/server.js
+++ bachend-development/nodejs/Todo-api-nodejs/server.js
@@ -2,15 +2,25 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
-const { log } = require("console");
 
-function readData() {
-  const dataString = fs.readFileSync("./notes.json", "utf-8");
-  return JSON.parse(dataString);
+function readData(callback) {
+  fs.readFile("./notes.json", "utf-8", (err, dataString) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, JSON.parse(dataString));
+    }
+  });
 }
 
-function writeData(data) {
-  fs.writeFileSync("./notes.json", JSON.stringify(data, null, 4));
+function writeData(data, callback) {
+  fs.writeFile("./notes.json", JSON.stringify(data, null, 4), (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null);
+    }
+  });
 }
 
 function readStaticFile(file, callback) {
@@ -32,20 +42,15 @@ const server = http.createServer((req, res) => {
   const match = pathname.match(/^\/api\/notes\/(\d+)$/);
 
   if (pathname == "/") {
-    const filePath = path.resolve(__dirname + "\\public", "index.html");
-    fs.readFile(filePath, (err, data) => {
+    readStaticFile("index.html", (err, data) => {
       if (err) {
-        log(err);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("500 Server Error");
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Not Found");
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(data);
       }
     });
-    // TODO: Implement route handling for:
-    // 1. Static files (HTML, CSS, JS)
-    // 2. API endpoints for notes (GET, POST, PUT, DELETE)
   } else if (pathname.startsWith("/public/")) {
     const fileName = pathname.replace("/public/", "");
     const contentType = fileName.endsWith(".css")
@@ -64,83 +69,129 @@ const server = http.createServer((req, res) => {
       }
     });
   } else if (pathname == "/api/notes" && req.method == "GET") {
-    const data = readData();
-    if (data) {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(data));
-    } else {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("no todos yet");
-    }
-  } else if (match && req.method == "GET") {
-    const data = readData().find((item) => item.id == Number(match[1]));
-    if (data) {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(data));
-    } else {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("404 Not Found");
-    }
-  } else if (pathname == "/api/notes" && req.method == "POST") {
-    const data = readData();
-    const lastId = data[data.length - 1]?.id || 0;
-    let body = "";
-
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", () => {
-      try {
-        const parsedData = JSON.parse(body);
-        const newtodo = {
-          title: parsedData.title,
-          id: Number(lastId) + 1,
-        };
-        data.push(newtodo);
-        writeData(data);
-      } catch (error) {
-        res.writeHead(422, { "Content-Type": "text/plain" });
-        res.end("Error parsing data:");
+    readData((err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 Server Error");
+      } else if (data) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(data));
+      } else {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("no todos yet");
       }
+    });
+  } else if (match && req.method == "GET") {
+    readData((err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 Server Error");
+      } else {
+        const note = data.find((item) => item.id == Number(match[1]));
+        if (note) {
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(note));
+        } else {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("404 Not Found");
+        }
+      }
+    });
+  } else if (pathname == "/api/notes" && req.method == "POST") {
+    readData((err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 Server Error");
+      } else {
+        const lastId = data[data.length - 1]?.id || 0;
+        let body = "";
 
-      res.writeHead(201, { "Content-Type": "text/plain" });
-      res.end("POST data received");
+        req.on("data", (chunk) => {
+          body += chunk.toString();
+        });
+
+        req.on("end", () => {
+          try {
+            const parsedData = JSON.parse(body);
+            const newtodo = {
+              title: parsedData.title,
+              id: Number(lastId) + 1,
+            };
+            data.push(newtodo);
+            writeData(data, (err) => {
+              if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("500 Server Error");
+              } else {
+                res.writeHead(201, { "Content-Type": "text/plain" });
+                res.end("POST data received");
+              }
+            });
+          } catch (error) {
+            res.writeHead(422, { "Content-Type": "text/plain" });
+            res.end("Error parsing data:");
+          }
+        });
+      }
     });
   } else if (match && req.method == "PUT") {
-    const data = readData();
-    let body = "";
+    readData((err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 Server Error");
+      } else {
+        let body = "";
 
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    req.on("end", () => {
-      try {
-        const parsedData = JSON.parse(body);
-        const todo = data.find((item) => item.id === Number(match[1]));
+        req.on("data", (chunk) => {
+          body += chunk.toString();
+        });
+        req.on("end", () => {
+          try {
+            const parsedData = JSON.parse(body);
+            const todo = data.find((item) => item.id === Number(match[1]));
 
-        if (parsedData.title) {
-          console.log("data2", todo);
-          todo.title = parsedData.title;
-          console.log("data3", todo);
-        }
+            if (parsedData.title) todo.title = parsedData.title;
 
-        writeData(data);
-      } catch (error) {
-        res.writeHead(422, { "Content-Type": "text/plain" });
-        res.end("Error parsing data:");
+            writeData(data, (err) => {
+              if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("500 Server Error");
+              } else {
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                res.end("PUT data received");
+              }
+            });
+          } catch (error) {
+            res.writeHead(422, { "Content-Type": "text/plain" });
+            res.end("Error parsing data:");
+          }
+        });
       }
-
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("PUT data received");
     });
   } else if (match && req.method == "DELETE") {
-    const data = readData();
-    const idx = data.findIndex((item) => item.id === Number(match[1]));
-    data.splice(idx, 1);
-    writeData(data);
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("data deleted successfully");
+    readData((err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 Server Error");
+      } else {
+        const idx = data.findIndex((item) => item.id === Number(match[1]));
+        if (idx !== -1) {
+          data.splice(idx, 1);
+          writeData(data, (err) => {
+            if (err) {
+              res.writeHead(500, { "Content-Type": "text/plain" });
+              res.end("500 Server Error");
+            } else {
+              res.writeHead(200, { "Content-Type": "text/plain" });
+              res.end("data deleted successfully");
+            }
+          });
+        } else {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("404 Not Found");
+        }
+      }
+    });
   } else {
     //Default response for unhandled routes
     res.writeHead(404, { "Content-Type": "text/plain" });
