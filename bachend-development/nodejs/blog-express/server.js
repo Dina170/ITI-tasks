a310c0bc57a5dd9a028ab4928f38e997db44e741
dina <dinaelhadidy966@gmail.com>
2025-04-11 23:58:02 +0200

    Set up middleware and routing for posts and users in server configuration

--- bachend-development/nodejs/blog-express/server.js
+++ bachend-development/nodejs/blog-express/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const postRoutes = require("./routes/postRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 mongoose
   .connect("mongodb://localhost:27017/blog")
@@ -15,3 +18,8 @@ mongoose
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
