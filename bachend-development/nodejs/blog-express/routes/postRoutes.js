b87eb100b0477b9062672fd1f757f2595e4ffdf8
dina <dinaelhadidy966@gmail.com>
2025-04-12 19:03:14 +0200

    Add validation middleware for creating and updating posts using Joi

--- bachend-development/nodejs/blog-express/routes/postRoutes.js
+++ bachend-development/nodejs/blog-express/routes/postRoutes.js
@@ -1,9 +1,23 @@
 const router = require("express").Router();
+const Joi = require("joi");
 const adminOnly = require("../middlewares/authRoles");
 const requireAuth = require("../middlewares/requireAuth");
 const Post = require("../models/Post");
 const User = require("../models/User");
 const AppError = require("../utils/AppError");
+const validate = require("../middlewares/validate");
+
+const createPostSchema = Joi.object({
+  title: Joi.string().required().min(3).max(100),
+  content: Joi.string().required().min(10).max(1000),
+  tags: Joi.array().items(Joi.string()).default([]),
+});
+
+const updatePostSchema = Joi.object({
+  title: Joi.string().min(3).max(100),
+  content: Joi.string().min(10).max(1000),
+  tags: Joi.array().items(Joi.string()),
+}).min(1);
 
 router.get("/", requireAuth, async (req, res, next) => {
   try {
@@ -38,22 +52,27 @@ router.get("/", requireAuth, async (req, res, next) => {
   }
 });
 
-router.post("/", requireAuth, async (req, res, next) => {
-  const { title, content, author, tags } = req.body;
-  try {
-    const post = new Post({
-      title,
-      content,
-      author,
-      tags,
-    });
+router.post(
+  "/",
+  requireAuth,
+  validate(createPostSchema),
+  async (req, res, next) => {
+    const { title, content, tags } = req.body;
+    try {
+      const post = new Post({
+        title,
+        content,
+        author: req.user._id,
+        tags,
+      });
 
-    await post.save();
-    res.status(201).json(post);
-  } catch (err) {
-    next(err);
+      await post.save();
+      res.status(201).json(post);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 router.get("/:id", requireAuth, adminOnly, async (req, res, next) => {
   try {
@@ -86,26 +105,31 @@ router.delete("/:id", requireAuth, async (req, res, next) => {
   }
 });
 
-router.put("/:id", requireAuth, async (req, res, next) => {
-  const { title, content, tags } = req.body;
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post) {
-      throw new AppError("Post not found", 404);
+router.put(
+  "/:id",
+  requireAuth,
+  validate(updatePostSchema),
+  async (req, res, next) => {
+    const { title, content, tags } = req.body;
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        throw new AppError("Post not found", 404);
+      }
+      if (!post.author.equals(req.user._id) && req.user.role !== "admin")
+        throw new AppError("not authorized", 403);
+
+      if (title) post.title = title;
+      if (content) post.content = content;
+      if (tags) post.tags = tags;
+
+      await post.save();
+      res.status(200).json(post);
+    } catch (err) {
+      next(err);
     }
-    if (!post.author.equals(req.user._id) && req.user.role !== "admin")
-      throw new AppError("not authorized", 403);
-
-    if (title) post.title = title;
-    if (content) post.content = content;
-    if (tags) post.tags = tags;
-
-    await post.save();
-    res.status(200).json(post);
-  } catch (err) {
-    next(err);
   }
-});
+);
 
 router.post("/:id/comments", requireAuth, async (req, res, next) => {
   const { text, author } = req.body;
