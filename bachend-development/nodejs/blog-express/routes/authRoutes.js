de1b567b76f8a69c14bc22920c935aa6238910d3
dina <dinaelhadidy966@gmail.com>
2025-04-12 18:50:27 +0200

    Add validation middleware for user registration and login with Joi

--- bachend-development/nodejs/blog-express/routes/authRoutes.js
+++ bachend-development/nodejs/blog-express/routes/authRoutes.js
@@ -1,8 +1,33 @@
 const { register, login } = require("../controllers/authController");
 
 const router = require("express").Router();
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+const validate = require("../middlewares/validate");
 
-router.post("/register", register);
-router.post("/login", login);
+const complexityOptions = {
+  min: 10,
+  max: 30,
+  lowerCase: 1,
+  upperCase: 1,
+  numeric: 1,
+  symbol: 1,
+  requirementCount: 2,
+};
+
+const loginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+const registerSchema = Joi.object({
+  email: Joi.string().required(),
+  password: passwordComplexity(complexityOptions),
+  username: Joi.string().min(3).max(30).required(),
+  role: Joi.string().valid("user", "admin").optional(),
+});
+
+router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
 
 module.exports = router;
