c2b936d78634c91c001f996ecfe8453d9f2bc4a3
dina <dinaelhadidy966@gmail.com>
2025-04-14 15:04:21 +0200

    Add products component with product listing and styles

--- frontend-development/angular/firstApp/src/app/app.component.ts
+++ frontend-development/angular/firstApp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
