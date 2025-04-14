47b1f2efa6c208d5aba4875bcbc6801a1fec2686
dina <dinaelhadidy966@gmail.com>
2025-04-14 15:36:12 +0200

    Add footer component with styles and image

--- frontend-development/angular/firstApp/src/app/app.component.ts
+++ frontend-development/angular/firstApp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductsComponent } from './products/products.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { CategoriesComponent } from './categories/categories.component';
     NavbarComponent,
     ProductsComponent,
     CategoriesComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ frontend-development/angular/firstApp/src/app/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {
+  imgUrl: string = 'images/7263263_3565075.jpg';
+}
