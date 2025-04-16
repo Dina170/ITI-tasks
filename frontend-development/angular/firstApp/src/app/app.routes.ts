18c2ef5139917c2efe3bdcd0ddec8b4af4d9f848
dina <dinaelhadidy966@gmail.com>
2025-04-16 21:32:54 +0200

    Redirect root path to home

--- frontend-development/angular/firstApp/src/app/app.routes.ts
+++ frontend-development/angular/firstApp/src/app/app.routes.ts
@@ -5,7 +5,8 @@ import { ContactComponent } from './contact/contact.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'products', component: ProductsListComponent },
