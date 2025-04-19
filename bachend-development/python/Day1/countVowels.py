1cb4a3991211db1316b31c06896c7ef7c9e4f9e6
dina <dinaelhadidy966@gmail.com>
2025-04-19 14:25:00 +0200

    Fix vowel counting to be case-insensitive and update test string

--- bachend-development/python/Day1/countVowels.py
+++ bachend-development/python/Day1/countVowels.py
@@ -1,8 +1,8 @@
-my_str = "dina ebrahim elhadidy"
+my_str = "dina ebrahim"
 
 count = 0
 for i in my_str:
-    if i in ["a", "e", "i", "o", "u"]:
+    if i.lower() in ["a", "e", "i", "o", "u"]:
         count += 1
 
 print(count);
