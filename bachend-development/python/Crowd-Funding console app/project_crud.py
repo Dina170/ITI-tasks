61e588fec1764b6712d4bb95ad3db9e8beaa29ea
dina <dinaelhadidy966@gmail.com>
2025-04-20 22:35:34 +0200

    Add edit project functionality with user authorization

--- bachend-development/python/Crowd-Funding console app/project_crud.py
+++ bachend-development/python/Crowd-Funding console app/project_crud.py
@@ -20,7 +20,7 @@ def add_project(userId):
         start = input("enter start time for the campaign: ")
         
     end = input("enter end time for the campaign: ")
-    while not validate_date(start):
+    while not validate_date(end):
         end = input("enter end time for the campaign: ")
         
     try:
@@ -42,8 +42,53 @@ def add_project(userId):
     else:
         add_item_to_file(data, "projects.json")
     
-def edit_project(userId, data):
-    pass
+def edit_project(userId):
+    project_id = input("enter project id: ")
+    while not project_id.isdigit():
+        print("project id must be number")
+        project_id = input("enter project id: ")
+    project_id = int(project_id)
+    
+    try:
+        projects = read_file("projects.json")
+    except FileNotFoundError:
+        print("no projects yet")
+    else:
+        found_project = search_project(project_id, projects)
+        if not found_project:
+            print("project not found")
+            return
+            
+        if userId != found_project["userId"]:
+            print("not authorized")
+            return
+        
+        title = input("enter new title: ") or found_project['title']
+        details = input("enter new details: ") or found_project['details']
+        
+        total_target = input("enter new total target") or str(found_project['total_target'])
+        while not total_target.isdigit():
+            total_target = input("enter total target: ")
+        
+        start = input("enter new start time: ") or found_project['start']
+        while not validate_date(start):
+            start = input("enter start time for the campaign: ")
+        
+        end = input("enter new end time: ") or found_project['end']
+        while not validate_date(end):
+            end = input("enter end time for the campaign: ")
+        
+        found_project.update({
+            "title": title,
+            "details": details,
+            "total_target": int(total_target),
+            "start": start,
+            "end": end
+        })
+        
+        write_file("projects.json", projects)
+        print("project updated successfully")
+        
         
 def delete_project(userId):
     project_id = input("enter project id: ")
@@ -62,6 +107,8 @@ def delete_project(userId):
             if userId == found_project["userId"]:
                 projects = [project for project in projects if project["id"] != project_id]
                 write_file("projects.json", projects)
+                print("project deleted successfully")
+                
             else:
                 print("not authorized")
         else:
