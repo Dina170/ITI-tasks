d5551318a00c8e0e43c1161049f49b0bea620fe5
dina <dinaelhadidy966@gmail.com>
2025-04-21 14:56:24 +0200

    Check is input is not alpha instead of checking if digit

--- bachend-development/python/Day1/validate_user.py
+++ bachend-development/python/Day1/validate_user.py
@@ -2,9 +2,12 @@ while True:
     name = input("enter name: ").strip()
     if not name:
         print("empty")
-    elif name.isdigit():
+    elif not name.isalpha():
         print("name can't be number");
     else:
         email = input("enter email: ").strip()
         break
         
+
+# /\w+@\w+\.\w{2,4}/
+# '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$
