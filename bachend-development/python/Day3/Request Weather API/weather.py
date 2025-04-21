01365912c5f3fdd7006cf34ba000fb4e44e562b5
dina <dinaelhadidy966@gmail.com>
2025-04-21 20:37:59 +0200

    Add function to retrieve latitude and longitude for a specified city

--- bachend-development/python/Day3/Request Weather API/weather.py
+++ bachend-development/python/Day3/Request Weather API/weather.py
@@ -13,5 +13,13 @@ def get_temperature_after(city, days, hour=None):
     temps = [{day.get("date"): day.get("hour")[0].get("temp_c")} for day in data]
     print(f'temp: {temps}')
     
+    
+def get_lat_and_long(city):
+    r = requests.get(f'http://api.weatherapi.com/v1/forecast.json?key=af1b64cb78ea4de0a34173742252104&q={city}')
+    data = r.json().get("location")
+    
+    print(f'lat: {data.get("lat")}, lon: {data.get("lon")}')
+    
 # get_current_temperature('Mansoura')
-get_temperature_after("Mansoura", 5, 6)
+# get_temperature_after("Mansoura", 5, 6)
+get_lat_and_long("Mansoura")
