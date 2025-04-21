be9eacbd96c57776ae5888f2259a1fd273e7646d
dina <dinaelhadidy966@gmail.com>
2025-04-21 18:45:55 +0200

    Enhance Queue class with size management and object tracking

--- bachend-development/python/Day3/Queue/queue.py
+++ bachend-development/python/Day3/Queue/queue.py
@@ -1,16 +1,28 @@
+from QueueOutOfRangeException import QueueOutOfRangeException
+
 class Queue:
-    def __init__(self):
+    objects = {}
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
         self.data = []
         self.front = self.rear = -1
+        Queue.objects[name] = []
 
     def is_empty(self):
-        return self.front == -1  
+        return self.front == -1 
+    
+    def is_full(self):
+        return self.rear == self.size - 1
 
     def insert(self, value):
+        if self.is_full():
+            raise QueueOutOfRangeException("queue is full")
         self.rear += 1
         if self.is_empty():
             self.front += 1
         self.data.append(value)
+        Queue.objects[self.name] = self.data[self.front:self.rear + 1]
 
     def pop(self):
         if self.is_empty():
@@ -20,8 +32,10 @@ class Queue:
         tmp = self.data[self.front]
         if self.front == self.rear:
             self.front = self.rear = -1
+            Queue.objects[self.name] = []
         else:
             self.front += 1
+            Queue.objects[self.name] = self.data[self.front:self.rear + 1]
         return tmp
         
     def print(self):
@@ -32,13 +46,16 @@ class Queue:
             print(self.data[i])
         
         
-# q1 = Queue()
-# q1.insert(2)
-# q1.insert(3)
-# q1.insert(4)
-# q1.print()
-# print("----------")
+q1 = Queue("test", 3)
+q2 = Queue("test2", 3)
+q1.insert(2)
+q1.insert(3)
+q1.insert(4)
+q2.insert(5)
+q1.print()
+print("----------")
 # q1.pop()
 # q1.pop()
 # q1.pop()
-# q1.print()
+q1.print()
+print(Queue.objects)
