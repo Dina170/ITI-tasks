77bdfcfafb439fcbc431440f413ee6830a9d0b2b
dina <dinaelhadidy966@gmail.com>
2025-04-21 14:57:15 +0200

    Turn string ro lowercase instead of turning each char to lowercase

--- bachend-development/python/Day1/count_vowels.py
+++ bachend-development/python/Day1/count_vowels.py
@@ -1,8 +1,8 @@
 my_str = "dina ebrahim"
 
 count = 0
-for i in my_str:
-    if i.lower() in ["a", "e", "i", "o", "u"]:
+for i in my_str.lower():
+    if i in ["a", "e", "i", "o", "u"]:
         count += 1
 
 print(count);
