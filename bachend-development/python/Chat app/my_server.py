b0bd48c05596cb697c737071699b49d953c3e1cd
dina <dinaelhadidy966@gmail.com>
2025-04-23 22:42:51 +0200

    Enhance WebSocket server to manage connected clients and broadcast client list

--- bachend-development/python/Chat app/my_server.py
+++ bachend-development/python/Chat app/my_server.py
@@ -1,19 +1,56 @@
 from web_socket_server import WebSocket, Server
+import json
 
 clients = []
 
 class MyChatApp(WebSocket):
+    connected_clients = {}  # Store username and connection pairs
+    
     def handle_message(self):
+        message = json.loads(self.data)
+        if message.get('messageType') == 'status':
+            if 'connected' in message.get('text'):
+                MyChatApp.connected_clients[message['user']] = self
+                # Broadcast updated client list
+                self.broadcast_client_list()
+            elif 'offline' in message.get('text'):
+                MyChatApp.connected_clients.pop(message['user'])
+                self.broadcast_client_list()
+        else:
+            self.broadcast_message(message)
+    
+    def broadcast_message(self, message):
         for client in clients:
             if client != self:
-                client.send_message(self.data)
-        # self.send_message("Connected to server")
-        
+                client.send_message(message)
+                
+    def broadcast_client_list(self):
+        client_list = list(MyChatApp.connected_clients.keys())
+        message = {
+            'messageType': 'clients',
+            'clients': client_list
+        }
+        for client in clients:
+            client.send_message(message)
+            
+    def send_message(self, message):
+        if isinstance(message, (dict, list)):
+            message = json.dumps(message)
+        super().send_message(message)
+            
     def handle_connected(self):
+        print(self)
         clients.append(self)
         
     def handle_close(self):
-        self.send_message("Connection closed")
+        # Remove client from connected clients
+        for username, client in MyChatApp.connected_clients.items():
+            if client == self:
+                MyChatApp.connected_clients.pop(username)
+                break
+        if self in clients:
+            clients.remove(self)
+        self.broadcast_client_list()
         
 
 server = Server(host='localhost', port=8080, websocketclass=MyChatApp)
