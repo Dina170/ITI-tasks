f625d79348f3e4b56b4a5f8a164df36caa5d49af
dina <dinaelhadidy966@gmail.com>
2025-04-25 15:23:26 +0300

    Get book images from static folder

--- bachend-development/django/book/book/books/urls.py
+++ bachend-development/django/book/book/books/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'books'
+
 urlpatterns = [
-    path('', views.books, name='books.index'),
-    path('<int:id>', views.book, name='books.book-display')
+    path('', views.books, name='index'),
+    path('<int:id>', views.book, name='book-display')
 ]
