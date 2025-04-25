452ea093a126e62ab2786f09c2e2f72ee664bfc0
dina <dinaelhadidy966@gmail.com>
2025-04-25 14:59:01 +0300

    Refactor book URLs and enhance styling for book display

--- bachend-development/django/book/book/books/urls.py
+++ bachend-development/django/book/book/books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.books, name='books-home'),
-    path("<int:id>", views.book, name='book-display')
+    path('', views.books, name='books.index'),
+    path('<int:id>', views.book, name='books.book-display')
 ]
