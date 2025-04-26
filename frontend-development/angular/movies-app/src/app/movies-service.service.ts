7e876ec387e9fe89fe00cee39e997bc7d8739325
dina <dinaelhadidy966@gmail.com>
2025-04-27 00:07:39 +0300

    Add MoviesService to handle API requests for trending movies

---
+++ frontend-development/angular/movies-app/src/app/movies-service.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesServiceService {
+  constructor(private _httpClient: HttpClient) {}
+
+  getTrending(): Observable<any> {
+    const headers = new HttpHeaders({
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzg2Zjg5NjczY2EzM2Y3ZTVjYzJlODA1MGQ4MzA3ZCIsIm5iZiI6MTc0NTY5MjIxOS4zMzksInN1YiI6IjY4MGQyNjNiZjc2OWYwYWY2YTgwZWZhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Dks578G8gy8-kr8TKerSbCkLgO-bsk6S-tSeAic8yT4',
+    });
+
+    return this._httpClient.get(
+      'https://api.themoviedb.org/3/trending/all/day?language=en-US',
+      { headers }
+    );
+  }
+}
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization:
+//       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzg2Zjg5NjczY2EzM2Y3ZTVjYzJlODA1MGQ4MzA3ZCIsIm5iZiI6MTc0NTY5MjIxOS4zMzksInN1YiI6IjY4MGQyNjNiZjc2OWYwYWY2YTgwZWZhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Dks578G8gy8-kr8TKerSbCkLgO-bsk6S-tSeAic8yT4',
+//   },
+// };
+
+// fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+//   .then((res) => res.json())
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
