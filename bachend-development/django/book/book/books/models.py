3692962a56dab46058260836273ad6ad3ce55773
dina <dinaelhadidy966@gmail.com>
2025-04-26 20:55:55 +0300

    Add Author and Book models with migrations, update admin registration, and modify .gitignore

--- bachend-development/django/book/book/books/models.py
+++ bachend-development/django/book/book/books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    publish_date = models.DateField()
+    add_to_site_at = models.DateTimeField(default=timezone.now)
+    price = models.FloatField()
+    appropriate = models.CharField(choices=[
+        ('under_8', 'under 8'),
+        ('8-15', '8-15'),
+        ('adults', 'adults')
+    ], default='8-15')
+    image = models.ImageField(default='default.jpg', upload_to='book_pics')
+    description = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+    
