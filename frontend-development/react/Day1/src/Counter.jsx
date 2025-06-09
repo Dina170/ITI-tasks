dc16714e8d79ef448fa813e419f3f5044470c4fa
dina <dinaelhadidy966@gmail.com>
2025-06-09 13:16:30 +0300

    Enhance CSS styles for improved layout and design

--- frontend-development/react/Day1/src/Counter.jsx
+++ frontend-development/react/Day1/src/Counter.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(1);
   return (
-    <div>
+    <div className="counter">
       <button onClick={() => setCount(count + 1)}>+</button>
       <p>{count}</p>
       <button onClick={() => setCount(count - 1)}>-</button>
